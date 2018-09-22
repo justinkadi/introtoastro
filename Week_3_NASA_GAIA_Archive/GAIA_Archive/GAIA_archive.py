1cd93ac2b5a3647a4d6336a5e73c2850005b831b
justinkadi <jeakadi@berkeley.edu>
2018-09-22 16:32:06 -0700

    Adding GAIA challenge

--- Week_3_NASA_GAIA_Archive/GAIA_Archive/GAIA_archive.py
+++ Week_3_NASA_GAIA_Archive/GAIA_Archive/GAIA_archive.py
@@ -3,8 +3,11 @@
 
 import pandas as pd
 import matplotlib.pyplot as plot
+import numpy as np
 
 gaia_data = pd.read_csv('GAIA_query_results.csv')
+
+plot.figure()
 
 plot.scatter(gaia_data.bp_rp, gaia_data.g_abs, s=.1, color='red')
 plot.ylim(30,-7)
@@ -23,4 +26,39 @@ plot.scatter(
 )
 
 plot.savefig('diagram')
+plot.show()
+
+
+""" Challenge question """
+
+new_data = pd.read_csv('GAIA_challenge_results.csv')
+
+plot.figure()
+
+heatmap, xedges, yedges = np.histogram2d(
+    new_data.bp_rp.values,
+    new_data.g_abs.values,
+    bins=100
+)
+
+extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+heatmap = np.ma.masked_where(heatmap == 0.0, heatmap)
+
+color_map = plot.cm.hot
+color_map.set_bad(color='white')
+
+plot.imshow(
+    np.sqrt(heatmap.T),
+    extent=extent,
+    cmap=color_map,
+    aspect=(extent[1]-extent[0])/(extent[3]-extent[2]),
+    origin='lower'
+)
+plot.colorbar()
+plot.xlabel('G$_{BP}$ - G$_{RP}$')
+plot.ylabel('M$_G$')
+plot.gca().invert_yaxis()
+
+plot.savefig('challenge_diagram')
 plot.show()
